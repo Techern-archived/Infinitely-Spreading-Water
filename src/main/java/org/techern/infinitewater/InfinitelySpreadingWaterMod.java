9e9610d5ab43feb32a1744f4aa834e0725510544
Cruz Julian Bishop <cruz@techern.org>
2017-04-10 12:32:56 +1000

    Version 1.0.0

--- src/main/java/org/techern/infinitewater/InfinitelySpreadingWaterMod.java
+++ src/main/java/org/techern/infinitewater/InfinitelySpreadingWaterMod.java
@@ -33,7 +33,7 @@ public class InfinitelySpreadingWaterMod
      *
      * @since 1.0.0
      */
-    public static final String VERSION = "1.0.0-SNAPSHOT";
+    public static final String VERSION = "1.0.0";
 
     /**
      * The {@link Logger} for {@link InfinitelySpreadingWaterMod}
@@ -81,8 +81,6 @@ public class InfinitelySpreadingWaterMod
     @EventHandler
     public void initialize(FMLInitializationEvent event)
     {
-        LOGGER.info("WE ARE A GO " + MOD_ID);
-
         GameRegistry.register(INFINITELY_SPREADING_WATER_BLOCK);
         GameRegistry.register(new ItemBlock(INFINITELY_SPREADING_WATER_BLOCK).setRegistryName(INFINITELY_SPREADING_WATER_BLOCK.getRegistryName()));
 
